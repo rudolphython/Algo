ca6092f359a6404b9b2a9e969efedd0c93612b93
rudolphython@gmail.com <rudolphython@gmail.com>
2022-01-25 21:10:07 +0900

    Create 0125practice

---
+++ 점프와순간이동.py
@@ -0,0 +1,19 @@
+"""
+출저 : 프로그래머스
+문제 : 점프와 순간이동
+"""
+
+def solution(n):
+    ans = 0
+    
+    while n > 1:
+        if n % 2 == 1:
+            n //= 2
+            ans += 1
+        elif n % 2 == 0:
+            n //= 2
+            
+    if n == 1:
+        ans += 1
+        
+    return ans
